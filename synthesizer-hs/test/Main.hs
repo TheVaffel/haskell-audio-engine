8659ff4cb70e7c4e72ae40332762d0be8d3b424f
Håkon Flatval <hkon20@hotmail.com>
2026-02-22 11:54:30 +0100

    Add test with some plotting

--- synthesizer-hs/test/Main.hs
+++ synthesizer-hs/test/Main.hs
@@ -1,13 +1,17 @@
 
 module Main where
 
-import Play (sinWave, playInterleave)
-
 import System.Exit (ExitCode)
 import Data.Functor (($>))
 
+import qualified Synthesizer.Storable.Signal as Sig
+import qualified Synthesizer.Generic.Cut as Cut
 
-main :: IO ExitCode
-main = playInterleave (\signal -> putStrLn "Hreee" $> signal) sinWave
--- main = playSplit sinWave
--- main = playWithIO (putStrLn "haha") sinWave
+import Design.Police
+
+main :: IO ()
+main = writeFile "./test_plot_data.txt" createString
+
+createString = let stream = exponentialOscillator 36.0 2.0
+               in
+                 unlines . map show  $ Sig.toList $ Cut.take 44100 stream
